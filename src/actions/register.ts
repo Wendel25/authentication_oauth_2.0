5a388f91dfbd9351454b6f6f05808f0f5c224c8c
Wendel25 <luccawendel25@gmail.com>
2024-10-09 16:48:42 -0300

    feat: implementation of user registration functionality

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,10 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from 'bcrypt';
+import { db } from '@/lib/db'
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const registerAction = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +13,26 @@ export const registerAction = async (values: z.infer<typeof RegisterSchema>) => 
         return { error: "Inválid credentials!" };
     };
 
-    return { success: "Email sent!" };
+    const { email, name, password } = validatedFields.data;
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: "Email already in use!" };
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashPassword,
+        }
+    })
+
+    // Verification token email
+
+
+    return { success: "User created!" };
 }
